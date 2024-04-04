44c962acf2776dadb4308925d52ad4ed6bf47439
kendastar1 <126899253+kendastar1@users.noreply.github.com>
2024-04-04 06:45:49 -0500

    Add files via upload

--- EjerciciosMatrices/src/main/java/com/mycompany/ejerciciosmatrices/EjerciciosMatrices.java
+++ EjerciciosMatrices/src/main/java/com/mycompany/ejerciciosmatrices/EjerciciosMatrices.java
@@ -19,6 +19,7 @@ public class EjerciciosMatrices {
     public static void main(String[] args) {
         int matriz[][] = null;
         int matriz2[][]=  null;
+        int suma = 0;
     
         
          System.out.println("Matriz 1 ");
--- EjerciciosMatrices/src/main/java/com/mycompany/ejerciciosmatrices/metodos.java
+++ EjerciciosMatrices/src/main/java/com/mycompany/ejerciciosmatrices/metodos.java
@@ -12,13 +12,17 @@ import java.util.Random;
  */
 public class metodos {
     public static void matriz1(int matriz[][]){
-   
-       
+     
+      
         Random aleatorio= new Random();
         matriz = new int [3][3];
+         int columnaSums []= new int[matriz[0].length];
+         int columnaSums1 []= new int[matriz[0].length];
           for(int a = 0; a < matriz.length;a++){
             for( int i = 0; i < matriz[a].length;i++){
                 matriz[a][i] = aleatorio.nextInt(15);
+                columnaSums[i] += matriz[a][i];
+                columnaSums1[a] += matriz[a][i];
                 
             }
         }
@@ -26,11 +30,24 @@ public class metodos {
          for(int a = 0; a < matriz.length;a++){
             for( int i = 0; i < matriz[a].length;i++){
                 System.out.print("[\t" + matriz[a][i] + "\t]\t");
+              
                 
             }
              System.out.println();
              
         }
+         for (int sum : columnaSums) {
+            System.out.println("La suma de las columnas es "+sum);
+            double promedio =(double) sum /3;
+            System.out.println("el promedio de la columna es " + promedio);
+        }
+           for (int sum1 : columnaSums1) {
+            System.out.println("La suma de las filas es "+sum1);
+            double promedio =(double) sum1 /3;
+            System.out.println("el promedio de la columna es " + promedio);
+        }
+         
+      
          
          
         
@@ -38,9 +55,13 @@ public class metodos {
     public static void matriz2(int matriz1[][]){
         Random aleatorio= new Random();
         matriz1 = new int [3][3];
+        int columnaSums []= new int[matriz1[0].length];
+         int columnaSums1 []= new int[matriz1[0].length];
           for(int a = 0; a < matriz1.length;a++){
             for(int i = 0; i < matriz1[a].length;i++){
                 matriz1[a][i] = aleatorio.nextInt(15);
+                 columnaSums[i] += matriz1[a][i];
+                 columnaSums1[a] += matriz1[a][i];
             }
         }
        
@@ -50,6 +71,16 @@ public class metodos {
                 System.out.print("[\t" + matriz1[a][i] + "\t]\t");
             }
              System.out.println();
+        }
+           for (int sum : columnaSums) {
+            System.out.println("La suma de las columnas es "+sum);
+            double promedio = (double)sum /3;
+            System.out.println("el promedio de la columna es " + promedio);
+        }
+           for (int sum1 : columnaSums1) {
+            System.out.println("La suma de las filas es "+sum1);
+             double promedio =(double) sum1 /3;
+            System.out.println("el promedio de la columna es " + promedio);
         }
     }
     
