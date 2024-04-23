80ccdff243bfbe8ef9671d163906a3dda5d6886a
kendastar1 <126899253+kendastar1@users.noreply.github.com>
2024-04-23 17:26:55 -0500

    Add files via upload

---
+++ FormulariosPaEntrega/src/formulario/metodos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.formulario;
+
+/**
+ *
+ * @author ESTUDIANTE19
+ */
+public class metodos {
+    public int resultado;
+    public void suma(int num1,int num2){
+        resultado = num1 + num2;
+    }
+     public void resta(int num1,int num2){
+        resultado = num1 - num2;
+    }
+      public void multiplicacion(int num1,int num2){
+        resultado = num1 * num2;
+    }
+       public void division(int num1,int num2){
+        resultado = num1 / num2;
+    }
+
+}
+
